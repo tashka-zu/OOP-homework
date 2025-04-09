5ca46cbf363d04576cde631c808f4e02718dffdb
«tashka-zu» <zubkovanatska21@gmail.com>
2025-04-09 17:36:57 +0300

    fixed src, tests, README

--- src/utils.py
+++ src/utils.py
@@ -7,13 +7,22 @@ class Product:
 
 
 class Category:
+    category_count = 0
+    product_count = 0
+
     def __init__(self, name: str, description: str):
         self.name = name
         self.description = description
         self.products = []
+        Category.category_count += 1
+
+    def add_product(self, product: Product):
+        self.products.append(product)
+        Category.product_count += 1
 
 
+# Пример использования
 category = Category("Электроника", "Электрические приборы")
 product = Product("Техника", "Ноутбук", 30000.39, 10)
 
-category.products.append(product)
+category.add_product(product)
