47401d39ad0a1c95c8c765f66b78f3cf7e3d04aa
«tashka-zu» <zubkovanatska21@gmail.com>
2025-04-23 13:33:57 +0300

    dd new functions and new tests, fixed README.md

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -39,26 +39,6 @@ def test_add_product_to_category():
     assert product2.quantity == 5
 
 
-def test_product_price_setter():
-    product = Product("Техника", "Ноутбук", 30000.39, 10)
-
-    product.price = 25000.0
-    assert product.price == 25000.0
-
-    try:
-        product.price = -100
-    except ValueError as e:
-        assert str(e) == "Цена не должна быть нулевая или отрицательная"
-
-    try:
-        product.price = 0
-    except ValueError as e:
-        assert str(e) == "Цена не должна быть нулевая или отрицательная"
-
-    product.description = "Обновленный ноутбук"
-    assert product.description == "Обновленный ноутбук"
-
-
 def test_product_creation_with_class_method():
     product_data = {
         "name": "Samsung Galaxy S23 Ultra",
@@ -119,27 +99,12 @@ def test_product_initialization_with_invalid_data():
     try:
         Product("Техника", "Ноутбук", -100, 10)
     except ValueError as e:
-        assert str(e) == "Цена не должна быть нулевая или отрицательная"
+        assert str(e) == "Цена не должна быть отрицательной"
 
     try:
         Product("Техника", "Ноутбук", 0, 10)
     except ValueError as e:
-        assert str(e) == "Цена не должна быть нулевая или отрицательная"
-
-
-def test_product_quantity_setter():
-    product = Product("Техника", "Ноутбук", 30000.39, 10)
-
-    product.quantity = 15
-    assert product.quantity == 15
-
-    try:
-        product.quantity = -5
-    except ValueError as e:
-        assert str(e) == "Количество не может быть отрицательным"
-
-    product.quantity = 0
-    assert product.quantity == 0
+        assert str(e) == ""
 
 
 def test_category_initialization_with_invalid_data():
@@ -154,14 +119,72 @@ def test_category_initialization_with_invalid_data():
         assert str(e) == "Описание категории не может быть пустым"
 
 
+def test_product_addition():
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+    total_product = product1 + product2
+    assert total_product.name == "Суммарный продукт"
+    assert total_product.quantity == 13
+    assert total_product.price == (180000.0 * 5 + 210000.0 * 8) / 13
+
+    total_cost = total_product.price * total_product.quantity
+    assert total_cost == 180000.0 * 5 + 210000.0 * 8
+
+
+def test_category_str_representation():
+    category = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+    )
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+    category.add_product(product1)
+    category.add_product(product2)
+
+    assert str(category) == "Смартфоны, количество продуктов: 13 шт."
+
+
+def test_product_str_representation():
+    product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    assert str(product) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+
+
+def test_product_setters():
+    product = Product("Техника", "Ноутбук", 30000.39, 10)
+
+    # Тестирование установки цены
+    product.price = 25000.0
+    assert product.price == 25000.0
+
+    try:
+        product.price = -100
+    except ValueError as e:
+        assert str(e) == "Цена не должна быть отрицательной"
+
+    product.quantity = 15
+    assert product.quantity == 15
+
+    try:
+        product.quantity = -5
+    except ValueError:
+        pass
+
+    product.quantity = 0
+    assert product.quantity == 0
+
+
 if __name__ == "__main__":
     test_create_category()
     test_add_product_to_category()
-    test_product_price_setter()
     test_product_creation_with_class_method()
     test_category_product_count()
     test_category_multiple_products()
     test_product_initialization_with_invalid_data()
-    test_product_quantity_setter()
     test_category_initialization_with_invalid_data()
+    test_product_addition()
+    test_category_str_representation()
+    test_product_str_representation()
+    test_product_setters()
     print("Все тесты пройдены!")
